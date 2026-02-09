7dc580431f9f44b120d64f69c564c46de8f39841
tejas17sep <tejasbachhao17sep@gmail.com>
2026-02-09 23:21:04 +0530

    Change

--- src/Inheritance/Inheritance1.java
+++ src/Inheritance/Inheritance1.java
@@ -1,5 +1,30 @@
 package Inheritance;
 
-public class Inheritance1 {
 
+class alpha{
+	
+	int age;
+	
+	void disp() {
+		System.out.println("Alpha Class");
+	}
+	
+}
+
+class Beta extends alpha{
+	
+	void show() {
+		System.out.println("Alpha Class");
+	}
+}
+
+public class Inheritance1 {
+	
+	public static void main(String [] args) {
+
+	Beta beta = new Beta();
+	
+	beta.disp();
+	
+	}
 }
