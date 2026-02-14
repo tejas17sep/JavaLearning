d70273cb0b693e8cdb92315f2af242f1332d3093
tejas17sep <tejasbachhao17sep@gmail.com>
2026-02-14 19:52:26 +0530

    1 public 2 protected 3 default 4 private

--- src/Inheritance/Inheritance5.java
+++ src/Inheritance/Inheritance5.java
@@ -1,9 +1,39 @@
 package Inheritance;
 
+class Telusko1 {
+	
+	int age =4;
+
+	void display() {
+		//age = 4;
+		System.out.println(age);
+	}
+
+}
+
+class Alean1 extends Telusko {
+	
+	int age = 10;
+
+	void display() {
+		// age=5;
+		System.out.println(age); // Super is use to call the value of age of parent class
+		//System.out.println(super.age); // Super is use to call the value of age of parent class
+	}
+	
+	void show() {
+		//super.display();
+		this.display();
+	}
+}
+
+
 public class Inheritance5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Telusko t = new Alean1();
+		((Alean1)t).show();
 
 	}
 
