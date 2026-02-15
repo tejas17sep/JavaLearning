618c06f6da987cb2eaa8de5abaf3477b3017675f
tejas17sep <tejasbachhao17sep@gmail.com>
2026-02-15 18:28:49 +0530

    Polimorphism Start

---
+++ src/Polimorphism/Poli1.java
@@ -0,0 +1,31 @@
+package Polimorphism;
+
+class AeroPlane{
+	
+	public void takeOff() {
+		System.out.println("Plane is Takeing Off");
+	}
+	public void fly() {
+		System.out.println("Plane is Flying");
+	}
+	
+}
+
+class FighterPlane extends AeroPlane{
+	
+}
+
+class CargoPlane extends AeroPlane{
+	
+	
+}
+
+
+
+public class Poli1 {
+	
+	public static void main(String[] args) {
+	
+	}
+
+}
