b89bb74db8598aaa7181c531eaf68f8107543089
tejas17sep <tejasbachhao17sep@gmail.com>
2026-02-17 19:52:19 +0530

    Loose Coupling

--- src/Polimorphism/InterfaceKeyword.java
+++ src/Polimorphism/InterfaceKeyword.java
@@ -1,26 +1,46 @@
 package Polimorphism;
 
-interface calc {  // all the methods of interface class in public and abstract
-	
-	void add(int a, int b);   // Ex : public abstract void add(int a, int b); 
-	
+interface calc { // all the methods of interface class in public and abstract
+
+	void add(int a, int b); // Ex : public abstract void add(int a, int b);
+
 	public abstract int mult(int a, int b);
 }
 
-class MyCal implements calc{
+class MyCal implements calc {
 
 	@Override
 	public void add(int a, int b) {
-		
-		
+
+		int c = a + b;
+		System.out.println("Add result is " + c);
+
 	}
 
 	@Override
 	public int mult(int a, int b) {
-		
-		return a*b;
+
+		return a * b;
 	}
-	
+
+}
+
+class MyCal2 implements calc {
+
+	@Override
+	public void add(int a, int b) {
+
+		int c = a + b + 100;
+		System.out.println("Add result is " + c);
+
+	}
+
+	@Override
+	public int mult(int a, int b) {
+
+		return a * b;
+	}
+
 }
 
 public class InterfaceKeyword {
